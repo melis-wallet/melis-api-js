288ebfc6365b753d25fc0f4e1f64488479adfd4d
Alessandro Polverini <alex@polverini.org>
2018-04-06 16:21:13 +0200

    Revamped examples

--- examples/create-multi-user2.js
+++ examples/create-multi-user2.js
@@ -1,7 +1,7 @@
-var CM = require('../src/cm')
-var C = CM.C
-var cm = new CM({apiDiscoveryUrl: C.MELIS_TEST_DISCOVER})
-var seed = cm.random32HexBytes()
+const Melis = require('../src/cm')
+const C = Melis.C
+const cm = new Melis({ apiDiscoveryUrl: C.MELIS_TEST_DISCOVER })
+const seed = cm.random32HexBytes()
 
 if (process.argv.length < 3) {
   console.log("Please pass joinCode as first argument")
@@ -9,16 +9,27 @@ if (process.argv.length < 3) {
 }
 var joinCode = process.argv[2]
 
-cm.log = function () {} // Disable logs
-cm.connect().then((config) => {
-  console.log("Connected to server. Blockchain height: " + config.topBlock.height + " joinCode: " + joinCode)
+// Disable melis logs
+cm.log = function () { }
+
+cm.on(C.EVENT_NEW_ACCOUNT, event => {
+  console.log("NEW Account: ", event)
+})
+
+cm.on(C.EVENT_ACCOUNT_UPDATED, event => {
+  console.log("Account event: ", event)
+})
+
+cm.connect().then(config => {
+  console.log("Connected to server")
   return cm.walletRegister(seed)
 }).then(wallet => {
   console.log("Wallet opened with seed: " + seed)
-  return cm.accountJoin({code: joinCode})
+  return cm.accountJoin({ code: joinCode })
 }).then(res => {
-  console.log("Multisig account joined with joinCode: " + joinCode)
-  return cm.disconnect()
+  console.log("Multisig account joined with joinCode: " + joinCode, res)
 }).catch(error => {
-  console.log("Unexpected exception: ", error)
+  console.log("Unexpected exception " + error.ex + " : " + error.message)
+}).finally(() => {
+  return cm.disconnect()
 })
